33212ea893aabfad62a4d5298a5a3d255eb700c1
Mike Green <mike.is.green@gmail.com>
2015-02-11 16:01:21 -0500

    stubbin'

---
+++ app/index.js
@@ -0,0 +1,22 @@
+/**
+ * FRC WordPress Generator
+ * Main Generator Class
+ */
+
+/* jshint node: true */
+
+'use strict';
+
+var _          = require('lodash');
+var generators = require('yeoman-generator');
+var yosay      = require('yosay');
+var request    = require('request');
+var chalk      = require('chalk');
+
+module.exports = generators.Base.extend({
+
+  constructor: function() {
+    generators.Base.apply(this, arguments);
+  }
+
+});
