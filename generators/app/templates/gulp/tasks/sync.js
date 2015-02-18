4073e449e13949663ec9934cdcb5ebd48f3935f2
Mike Green <mike.is.green@gmail.com>
2015-02-18 12:14:25 -0500

    wrote sync tasks

---
+++ generators/app/templates/gulp/tasks/sync.js
@@ -0,0 +1,84 @@
+/**
+ * Staging Synchronization Gulp Tasks
+ */
+
+/* jshint node: true */
+
+'use strict';
+
+var path    = require('path');
+var root    = path.join(__dirname, '..', '..');
+var secrets = require(path.join(root, 'secrets.json'));
+var gulp    = require('gulp');
+var helpers = require(path.join(root, 'gulp', 'lib', 'helpers'));
+var chalk   = require('chalk');
+var $       = require('gulp-load-plugins')({ lazy: true });
+var _       = require('lodash');
+var staging = secrets.servers.staging.rsync;
+
+var hasValid = function(obj, key) {
+  return _.has(obj, key) && !_.isEmpty(obj[key]);
+};
+
+var isValidDirection = function(obj, key) {
+  return hasValid(obj, key) && (obj[key] === 'up' || obj[key] === 'down');
+};
+
+var syncTask = function(opts) {
+  var config     = secrets.servers.staging.rsync;
+  opts           = _.isObject(opts) ? opts : {};
+  opts.source    = hasValid(opts, 'source') ? opts.source : path.join(root, 'build/');
+  opts.dest      = hasValid(opts, 'dest') ? opts.dest : config.path;
+  opts.message   = hasValid(opts, 'message') ? opts.message : 'Beginning sync task';
+  opts.direction = isValidDirection(opts, 'direction') ? opts.direction : 'up';
+
+  var rsync = [
+    'rsync -avzr',
+    '-e "ssh -p <%= staging.ssh.port %>"',
+    '--delete'
+  ];
+
+  if (opts.direction === 'up') {
+    rsync.push('<%= source %>');
+    rsync.push('<%= staging.rsync.username %>@<%= staging.rsync.hostname %>:<%= dest %>');
+  } else {
+    rsync.push('<%= staging.rsync.username %>@<%= staging.rsync.hostname %>:<%= source %>');
+    rsync.push('<%= dest %>');
+  }
+
+  return function() {
+    $.util.log(chalk.magenta(opts.message));
+
+    return gulp.src('')
+      .pipe(helpers.shell(rsync, { source: opts.source, dest: opts.dest }));
+  };
+};
+
+gulp.task('sync:up', syncTask({ message: 'Syncing up to staging...' }));
+
+gulp.task('uploads:up', syncTask({
+  source: path.join(root, 'uploads/'),
+  dest: path.join(staging.public_html, 'wp-content', 'uploads/'),
+  message: 'Syncing uploads UP...'
+}));
+
+gulp.task('uploads:down', syncTask({
+  source: path.join(staging.public_html, 'wp-content', 'uploads/'),
+  dest: path.join(root, 'uploads/'),
+  message: 'Syncing uploads DOWN...',
+  direction: 'down'
+}));
+
+gulp.task('plugins:up', syncTask({
+  source: path.join(root, 'plugins/'),
+  dest: path.join(staging.public_html, 'wp-content', 'plugins/'),
+  message: 'Syncing plugins UP...',
+  direction: 'up'
+}));
+
+gulp.task('plugins:down', syncTask({
+  source: path.join(staging.public_html, 'wp-content', 'plugins/'),
+  dest: path.join(root, 'plugins/'),
+  message: 'Syncing plugins DOWN...',
+  direction: 'down'
+}));
