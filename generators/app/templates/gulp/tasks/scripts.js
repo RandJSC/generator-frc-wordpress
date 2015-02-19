bd1cfcd3615a09d0593cd18fcf36d97d7f489366
Mike Green <mike.is.green@gmail.com>
2015-02-19 12:15:27 -0500

    Created JS compilation pipeline that should work

--- generators/app/templates/gulp/tasks/scripts.js
+++ generators/app/templates/gulp/tasks/scripts.js
@@ -16,6 +16,23 @@ var browserify  = require('browserify');
 var runSequence = require('run-sequence');
 var source      = require('vinyl-source-stream');
 var buffer      = require('vinyl-buffer');
+var lazypipe    = require('lazypipe');
+
+var jsPipeline  = function(filename) {
+  var output = path.join(config.build, 'js');
+
+  return lazypipe()
+    .pipe(source, filename)
+    .pipe(buffer)
+    .pipe($.sourcemaps.init, { loadMaps: true })
+    .pipe(function() {
+      return $.if(config.production, $.uglify());
+    })
+    .pipe($.sourcemaps.write, output)
+    .pipe(gulp.dest, output)
+    .pipe($.gzip)
+    .pipe(gulp.dest, output);
+};
 
 gulp.task('scripts', function(cb) {
   runSequence('scripts:main', cb);
@@ -27,13 +44,5 @@ gulp.task('scripts:main', function() {
     debug: !config.production
   });
 
-  var bundle = function() {
-    return bundler
-      .bundle()
-      .pipe(source('main.js'))
-      .pipe(buffer())
-      //.pipe($.if(!config.production, $.sourcemaps.init({ loadMaps: true })))
-      .pipe($.if(config.production, $.uglify()))
-      .pipe(gulp.dest(path.join(root, 'build', 'js/')));
-  };
+  return bundler.bundle().pipe(jsPipeline('main.js'));
 });
