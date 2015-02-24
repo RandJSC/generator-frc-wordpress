db7602ac45f95f0a8185407d1765f7f4a787532b
Mike Green <mike.is.green@gmail.com>
2015-02-24 12:59:15 -0500

    Made JS tasks into a reusable function

--- generators/app/templates/gulp/tasks/scripts.js
+++ generators/app/templates/gulp/tasks/scripts.js
@@ -21,17 +21,21 @@ var buffer      = require('vinyl-buffer');
 var browserSync = require('browser-sync');
 var reload      = browserSync.reload;
 
+var jsTask      = function(filename) {
+  return function() {
+    var bundler = browserify({
+      entries: [ path.join(root, 'source', 'js', filename) ],
+      debug: !config.production
+    }).transform(babelify);
+
+    return bundler.bundle()
+      .pipe(pipelines.javascript(filename)())
+      .pipe(reload({ stream: true }));
+  };
+};
+
 gulp.task('scripts', function(cb) {
   runSequence('scripts:main', cb);
 });
 
-gulp.task('scripts:main', function() {
-  var bundler = browserify({
-    entries: [ path.join(root, 'source', 'js', 'main.js') ],
-    debug: !config.production
-  }).transform(babelify);
-
-  return bundler.bundle()
-    .pipe(pipelines.javascript('main.js')())
-    .pipe(reload({ stream: true }));
-});
+gulp.task('scripts:main', jsTask('main.js'));
